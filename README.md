745432e441fad0d875f9520eb52b21101ab7d486
AugustDay <augustday.adi@gmail.com>
2016-12-14 21:32:34 -0800

    Updated simulator for MIPs language.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # 372-mips
 https://canvas.uw.edu/courses/1070035/assignments/3483633
+
+1. Write a small MIPS assembly program to load two integer numbers from memory. Add them up. Then store the result back to memory. Save the source code to a file;
+
+2. Manually translate the above MIPS assembly code to MIPS machine code (32 bits);
+
+3. Write a C program to load the above assembly source code and translate it to machine code, load the machine code to memory, then execute the machine code. The C program should model the datapath of a MIPS computer, i.e., it should have an ALU, a register file, I-Mem, D-Mem, pipeline registers, and a PC. Take screenshots of the memory map to show that your program worked.   
---
+++ fsm.c
@@ -0,0 +1,358 @@
+/*
+	fsm.c
+	
+   Austin Ingraham
+   Gabriel Summers
+	Date: 12/12/16
+	Description:
+		This file contains the implementation code for simulating a Finite State Machine
+        based on the MIPs language.
+*/
+
+#include "cpu.h"
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define FETCH 0
+#define DECODE 1
+#define EXECUTE 2
+#define MEMACCESS 3
+#define WRITEBACK 4
+#define HALTINSTRUCTION 5
+#define MEMORY_SIZE 0xFFFF
+
+unsigned int memory[MEMORY_SIZE]; 
+
+void purgeBuffer() {
+   char c; 
+   c = getchar();
+
+   while(c != '\n' && c != EOF) {
+      c = getchar();  
+   }
+}
+
+void displayMemory(CPU_p cpu, int m) {
+   int i = 0;
+   char c = 'N';
+   printf("\nRegisters\tMemory\n==================================\n");
+   for(i; i < NO_OF_REGISTERS; i++, m += 2) {
+      printf("R%d: %.8X\t", i, getRegister(cpu, i));
+      printf("0x%.4X: %.8X  %.8X\n", m, memory[m], memory[m + 1]);
+   }
+   if (cpu->sw == 1) { 
+      c = 'P'; 
+   } else if (cpu->sw == 2) {
+      c = 'Z';      
+   } 
+   printf("\nPC: %.4X   SW: %c   IR: %.8X\n", cpu->pc, c, getIR(cpu));
+}
+
+FILE* openFile(char filename[]) {
+    FILE* file = fopen(filename, "r");
+
+    if (!file) {
+        printf("\tCould not open filename \"%s\"\n\n", filename);
+        return getInputFile();
+    } else {
+        return file;
+    }
+}
+
+FILE* getInputFile() {
+    int i = 0, size = 50; //max allowed filename
+    char ch, filename[size];  
+
+    printf("\tEnter the name of the file to load:\n\t> ");
+    scanf("%c", &ch);
+    while (ch != '\n' && i < size - 1) { //builds string from chars entered
+        filename[i] = ch;
+        ch = getchar();
+        i++;
+    }    
+    filename[i] = '\0';
+    return openFile(filename);
+}
+
+int loadData() {
+	int i = 0;
+   char line[10];
+   FILE* file = getInputFile();
+   printf("\tFound the file, loading data... ");
+   // fgets(line, sizeof(line), file);
+   // Register startingIndex = strtol(line, NULL, 16);
+   // int i = startingIndex;
+   while (fgets(line, sizeof(line), file) && i < MEMORY_SIZE) {
+      memory[i] = strtol(line, NULL, 16);
+      i++;
+   }
+   printf("done.\n");
+   //return startingIndex;  
+   return 1;
+}
+
+int saveData(int m) {
+	FILE* outFile = fopen("output.hex", "w");
+	int i = 0;
+	if (!outFile) {
+    	printf("\tERROR OPENING FILE\n");
+		return FILE_ERROR;
+    }
+    for(i; i <= m; i++) {
+	   fprintf(outFile, "%.8X\n", memory[i]); 		   
+    }
+	fclose(outFile);
+	return 1;
+}
+
+int debug(CPU_p cpu) {
+   int m = cpu->pc, input = 8, repeat = 1, run = 0;;
+   unsigned short reg = 0;
+   char* memoryLoc = malloc(sizeof(char) * 20);
+   displayMemory(cpu, m);
+   while(input > 0) {      
+      if (m > (MEMORY_SIZE - 16)) {
+         m = MEMORY_SIZE - 16;   
+      }
+      printf("\n1 Load, 2 Save, 3 Run, 4 Step, 5 Memory Dump, 6 Memory Edit, 7 Reset\n> ");
+      scanf(" %d", &input);
+      purgeBuffer();
+      switch(input) {
+         case 1: //LOAD OBJECT FILE
+			loadData();
+			displayMemory(cpu, m);
+            // cpu->pc = loadData();   
+            // displayMemory(cpu, cpu->pc);
+            break;
+		 case 2:
+			printf("\tChoose terminating index of memory to output, (0 - %d)\n\t> ", (MEMORY_SIZE - 1));
+			scanf(" %d", &m);
+            if(m < 0) {
+               m = 0;
+            } else if (m > (MEMORY_SIZE - 1)) {
+               m = MEMORY_SIZE - 1;   
+            }
+			printf("\tSaving memory into output.hex ... ");
+			saveData(m);
+			printf("done.\n");
+			break;
+		 case 3:
+		    printf("\tRunning...\n");
+			run = 1;
+			input = 0;
+			break;
+         case 4: //STEP
+            input = 0;
+            break;
+         case 5: //MEMORY DUMP
+            printf("\tChoose memory dump starting index, (0 - %d)\n\t> ", (MEMORY_SIZE - 16));
+            scanf(" %d", &m);
+            if(m < 0) {
+               m = 0;
+            } else if (m > (MEMORY_SIZE - 16)) {
+               m = MEMORY_SIZE - 16;   
+            }
+            displayMemory(cpu, m);
+
+            break;
+         case 6: //MEMORY EDIT
+            repeat = 1;
+            while(repeat) {
+               printf("\tEnter memory location to edit,\n\t> ");
+               scanf(" %s", memoryLoc);
+               m = strtol(memoryLoc, NULL, 0);
+               if(m < 0 || m >= MEMORY_SIZE) {
+                  printf("\tIndex out of range, please enter value between 0 and %d.\n",
+                         (MEMORY_SIZE - 1));
+               } else {
+                  printf("\t%d/%.4X location chosen. Please enter new value:\n\t> ", m, m);
+                  scanf(" %s", memoryLoc);
+                  memory[m] = strtol(memoryLoc, NULL, 16);
+                  printf("\tContinue editing data? y/n\n\t> ");
+                  scanf(" %s", memoryLoc);
+                  if(memoryLoc[0] == 'y' || memoryLoc[0] == 'Y') {} else { repeat = 0;}
+               }
+            }
+            
+            break;
+         case 7: //RESET
+            printf("\tResetting simulator, y to confirm,\n\t> ");
+            scanf(" %s", memoryLoc);
+            if(memoryLoc[0] == 'y' || memoryLoc[0] == 'Y') {
+               printf("\tResetting... ");
+               initMemory();
+               initRegisters(cpu);
+               resetCPU(cpu);
+               printf("done.\n");
+               displayMemory(cpu, m);
+            } 
+            break;
+         default:
+            printf("\tInvalid choice, please enter value 1-7\n");
+            input = 8;
+            break;
+      }
+      
+   }
+   free(memoryLoc);
+   return run;
+}
+
+void initRegisters(CPU_p cpu) {
+   int i = 0;
+   for(i; i < NO_OF_REGISTERS; i++) {
+	   cpu->reg_file[i] = 0;	
+   }
+}
+
+void initMemory() {
+   int i = 0;
+   for(i; i < MEMORY_SIZE; i++) {
+	   memory[i] = 0x00000000;	
+   }
+   memory[i] = 0xFFFFFFFF;
+}
+
+//=========================================================================================
+int controller (CPU_p cpu) {
+    if (cpu == NULL) return POINTER_ERROR;
+    if (cpu->alu == NULL) return POINTER_ERROR;
+	 unsigned int state = FETCH;
+	 Byte opcode = 0, rd = 0, rs = 0, rt = 0, immed;
+	 int run = 0;
+	 initMemory();
+	 initRegisters(cpu); 
+    for(;;) {   // efficient endless loop
+        switch (state) {
+            case FETCH: 
+				if (run == 0) {
+					run = debug(cpu);
+				}
+                cpu->mar = cpu->pc;  
+                cpu->mdr = memory[cpu->mar];   
+                cpu->ir = cpu->mdr;      
+                cpu->pc++;       
+                state = DECODE;
+                break;
+            case DECODE:
+                opcode = getOPCODE(cpu);
+                switch (opcode) {
+                    case ADD:
+                        rd = getRD(cpu);
+                        rs = getRS(cpu);
+                        rt = getRT(cpu);
+                        break;
+                    case LW:
+                        rs = getRS(cpu);			
+                        rt = getRT(cpu);
+                        immed = getIMMED(cpu);
+                        break;
+                    case LA:		
+                        rt = getRT(cpu);
+                        immed = getIMMED(cpu);
+                        break;
+                    case SW:
+                        rt = getRT(cpu);
+                        rs = getRS(cpu);
+                        immed = getIMMED(cpu);
+                        break;
+                    default: break;
+                }
+                state = EXECUTE;
+                break;
+            case EXECUTE:
+                switch (opcode) {
+                   case ADD: 
+                      cpu->alu->a = cpu->reg_file[rs];
+                      cpu->alu->b = cpu->reg_file[rt];
+                      cpu->mdr = alu_ADD(cpu);
+                      break;
+                   case LW:
+                      cpu->alu->a = cpu->reg_file[rs];
+                      cpu->alu->b = immed/4;
+                      cpu->mar = alu_ADD(cpu);
+                      break;
+                   case LA:
+                      cpu->mar = immed;
+                      break;
+                   case SW:
+                      cpu->alu->a = cpu->reg_file[rs];
+                      cpu->alu->b = immed/4;
+                      cpu->mar = alu_ADD(cpu);
+                      break;
+                   default: break;
+                }
+              
+                state = MEMACCESS;
+                break;
+            case MEMACCESS:
+                switch (opcode) {
+                   case ADD: break;
+                   case LW:
+                      /*Load the target register with the data in cpu->mdr. */
+                      cpu->mdr = memory[cpu->mar];
+                      break;
+                   case LA:
+                      /*Load the target register with the value of alu->r. */
+                      cpu->mdr = memory[getALU_R(cpu->alu)];
+                      break;
+                   case SW:
+                      memory[cpu->mar] = cpu->reg_file[rt];
+                      break;
+                   default: break;
+                }
+
+                state = WRITEBACK;
+                break;
+            case WRITEBACK:
+                switch (opcode) {
+                   case ADD:
+                      cpu->reg_file[rd] = cpu->mdr;
+                      break;
+                   case LW:
+                      cpu->reg_file[rt] = cpu->mdr;
+                      break;
+                   case LA:
+                      cpu->reg_file[rd] = cpu->mar;
+                      break;
+                   case SW: break;
+                   default:
+                      if(cpu->ir == 0xFFFFFFFF){
+                          printf("HALT REACHED\n");
+                          exit(0);
+                      }
+                      break;
+                }                        
+                //zero-out everything                          
+                rd = 0;
+                rs = 0;
+                rt = 0;
+                opcode = 0;
+                cpu->alu->a = 0;
+                cpu->alu->b = 0;
+                state = FETCH;   
+                break;
+        }
+    }
+    displayMemory(cpu, 0);
+    return 1;
+}
+
+
+/* Driver for FSM */
+int main (int argc, char *argv[]) {
+    //PREPARE CPU 
+    CPU_p cpu = constructCPU();
+    initCPU(cpu);
+
+    if(controller(cpu) == 1) {
+        printf("Operations completed successfully.\n");	
+    } else {
+        printf("Something went wrong.\n");
+    } 
+
+    return 0;
+}
